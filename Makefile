99e1b78ebc83771a8c5d9adb17a6faeccfb8ae21
Edward Rudd <urkle@outoforder.cc>
2012-11-21 19:50:22 -0500

    add shell wrapper and add log file support

--- Makefile
+++ Makefile
@@ -1,2 +1,14 @@
-file_monitor.so: file_monitor.h
-	gcc -FPIC -shared -ldl -o $> $<
+all: file_monitor32.so file_monitor64.so
+
+file_monitor32.so: file_monitor.c
+	gcc -m32 -fPIC -shared -ldl -o $@ $^
+
+file_monitor64.so: file_monitor.c
+	gcc -m64 -fPIC -shared -ldl -o $@ $^
+
+install: file_monitor32.so file_monitor64.so
+	cp file_monitor32.so file_monitor64.so ~/bin/file_monitor-libs/
+	cp file_monitor ~/bin/
+
+clean:
+	rm -f file_monitor32.so file_monitor64.so
--- file_monitor.c
+++ file_monitor.c
@@ -1,16 +1,48 @@
 #define _GNU_SOURCE
 #include <stdio.h>
+#include <stdlib.h>
 #include <dlfcn.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdarg.h>
 
-int open(const char *fn, int flags) {
-    static int (*real_open)(const char *fn, int flags);
+int open(const char *fn, int flags, ...) {
+    static int (*real_open)(const char *fn, int flags, ...);
+    static int envChecked = 0;
+    static FILE* logFP = NULL;
 
     if (!real_open) {
         real_open = dlsym(RTLD_NEXT, "open");
     }
+    
+    if (envChecked == 0) {
+        envChecked = 1;
+        char *logfile = getenv("FILE_MONITOR_LOG");
+        if (logfile) {
+            int logFD = real_open(logfile, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
+            logFP = fdopen(logFD, "w");
+        }
+    }
 
-    fprintf(stderr, "opened file '%s'\n", fn);
-    return real_open(fn, flags);
+    char m[4] = { '\0', '\0', '\0' };
+    m[0] = (flags & O_CREAT) ? 'c' : '_';
+    m[1] = (flags & O_WRONLY) ? 'w' : '_';
+    m[2] = (flags & O_APPEND) ? 'a' : '_';
+
+    if (logFP) {
+        fprintf(logFP, "opened file '%s' - %s\n", fn, m);
+    } else {
+        fprintf(stderr, "opened file '%s' - %s\n", fn, m);
+    }
+    if (flags & O_CREAT) {
+        va_list ap;
+        va_start(ap, flags);
+        mode_t mode = va_arg(ap, mode_t);
+        va_end(ap);
+        return real_open(fn, flags);
+    } else {
+        return real_open(fn, flags);
+    }
 }
 
 // vim: et ts=4
