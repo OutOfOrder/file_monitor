3623f2ccf07a0a06dfd6638f85d39c21a3484872
Edward Rudd <urkle@outoforder.cc>
2012-12-11 21:11:04 -0500

    add logging for stat function

--- file_monitor.c
+++ file_monitor.c
@@ -10,6 +10,9 @@ static int (*real_open)(const char *fn, int flags, ...);
 static int (*real_open64)(const char *fn, int flags, ...);
 static FILE* (*real_fopen)(const char*__restrict fn, const char*__restrict mode);
 static FILE* (*real_fopen64)(const char*__restrict fn, const char*__restrict mode);
+static int (*real_stat)(const char *__restrict path, struct stat *__restrict buf);
+static int (*real_stat64)(const char *__restrict path, struct stat64 *__restrict buf);
+
 static short startup = 0;
 static FILE* logFP = NULL;
 
@@ -25,6 +28,8 @@ void startup_check()
     real_open64 = dlsym(RTLD_NEXT, "open64");
     real_fopen = dlsym(RTLD_NEXT, "fopen");
     real_fopen64 = dlsym(RTLD_NEXT, "fopen64");
+    real_stat = dlsym(RTLD_NEXT, "stat");
+    real_stat64 = dlsym(RTLD_NEXT, "stat64");
 
     char *logfile = getenv("FILE_MONITOR_LOG");
     if (logfile) {
@@ -38,6 +43,11 @@ void startup_check()
 void log_open(const char* func, const char* fn, const char* mode)
 {
     fprintf(logFP, "opened file %s('%s','%s')\n", func, fn, mode);
+}
+
+void log_other(const char* func, const char* fn, const char* extra)
+{
+    fprintf(logFP, "called %s('%s') == '%s'", func, fn, extra);
 }
 
 int open(const char *fn, int flags, ...)
@@ -106,6 +116,28 @@ FILE* fopen64(const char* fn, const char* modes)
     log_open("fopen64", fn, modes);
 
     return real_fopen64(fn, modes);
+}
+
+int stat(const char* path, struct stat* buf)
+{
+    startup_check();
+
+    int ret = real_stat(path, buf);
+
+    log_other("stat", path, ret == 0 ? "OK" : "!found");
+
+    return ret;
+}
+
+int stat64(const char* path, struct stat64* buf)
+{
+    startup_check();
+
+    int ret = real_stat64(path, buf);
+
+    log_other("stat", path, ret == 0 ? "OK" : "!found");
+
+    return ret;
 }
 
 // vim: et ts=4
